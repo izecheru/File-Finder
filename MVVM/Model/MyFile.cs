a7b4f642af8085779d08982b484746c29ab28fc5
Zecheru Ionut <56791024+izecheru@users.noreply.github.com>
2023-05-23 18:45:28 +0300

    List clears at every update
    Now the list issue is patched. This project is kind of bad but i'll keept it public.

--- MVVM/Model/MyFile.cs
+++ MVVM/Model/MyFile.cs
@@ -1,20 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Find_Dupes.MVVM.Model
+﻿namespace Find_Dupes.MVVM.Model
 {
     internal class MyFile
     {
         private string _name;
         private string _type;
         private string _path;
-        public string Name { get { return _name; } set { _name = value; } }
-        public string Type { get { return _type; } set { _type = value; } }
-        public string Path { get { return _path; } set { _path = value; } }
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public string Type
+        {
+            get
+            {
+                return _type;
+            }
+            set
+            {
+                _type = value;
+            }
+        }
+        public string Path
+        {
+            get
+            {
+                return _path;
+            }
+            set
+            {
+                _path = value;
+            }
+        }
 
         public MyFile(string path)
         {
--- MVVM/ViewModel/MainView_ViewModel.cs
+++ MVVM/ViewModel/MainView_ViewModel.cs
@@ -2,113 +2,59 @@
 using Find_Dupes.MVVM.Model;
 using Ookii.Dialogs.Wpf;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Security.Permissions;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
-using static System.Net.WebRequestMethods;
 
 namespace Find_Dupes.MVVM.ViewModel
 {
     internal class MainView_ViewModel : ObservableObject
     {
-        private ObservableCollection<MyFile> _myFiles;
-        private string _fileTypeFilter;
         private int _currentNumberOfFiles;
+        private CollectionViewSource _fileCollection;
+        private string _fileTypeFilter;
+        private string _filterText;
+        private ObservableCollection<MyFile> _myFiles;
         private string _selectedItemListView;
 
-        public string SelectedItemListView
-        {
-            get { return _selectedItemListView; }
-            set { _selectedItemListView = value; OnPropertyChanged();OnSelectedItemListViewChanged(); }
-        }
-
-        private void OnSelectedItemListViewChanged()
-        {
-            MyFile file = new MyFile(_selectedItemListView);
-            string temp;
-            int index=0;
-            for(int i=file.Path.Length-1;i>=0 ;i--)
-            {
-                if (file.Path[i]=='\\')
-                {
-                    index = i; break;
-                }
-            }
-            Process.Start("explorer.exe", file.Path.Substring(0, index));
-            //Clipboard.SetText(file.Path.Substring(0,index));
-        }
-
-        public string FileTypeFilter
-        {
-            get { return _fileTypeFilter; }
-            set { _fileTypeFilter = value; OnPropertyChanged(); }
-        }
-
-        public int CurrentNumberOfFiles
-        {
-            get { return _currentNumberOfFiles; }
-            set { _currentNumberOfFiles = value; OnPropertyChanged(); }
-        }
-        public ICommand GetAllFilesCommand { get; }
-
-        private string _filterText;
-        private CollectionViewSource _fileCollection;
         public MainView_ViewModel()
         {
-            GetAllFilesCommand = new RelayCommand(ExecuteGetAllFiles);
+            GetAllFilesCommand = new RelayCommand(o => ExecuteGetAllFiles());
             _myFiles = new ObservableCollection<MyFile>();
             _fileCollection = new CollectionViewSource();
             _fileCollection.Source = _myFiles;
             _fileCollection.Filter += FileCollectionFilter;
         }
 
-
-        private void ExecuteGetAllFiles(object obj)
-        {
-            VistaFolderBrowserDialog dialog = new VistaFolderBrowserDialog();
-            dialog.SelectedPath = "";
-            dialog.UseDescriptionForTitle = true;
-            dialog.ShowNewFolderButton = true;
-            dialog.Description = @"Select target folder";
-            dialog.ShowDialog();
-            GetAllFiles(dialog.SelectedPath);
-        }
-
-        private void GetAllFiles(string folderPath)
-        {
-            try
-            {
-                string[] allfiles = Directory.GetFiles(folderPath, "*.*", SearchOption.AllDirectories);
-                foreach (string file in allfiles)
-                {
-                    _myFiles.Add(new MyFile(file));
-                }
-                CurrentNumberOfFiles = _myFiles.Count;
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex);
-            }
-        }
-
-
-        public ICollectionView SourceCollection
+        public int CurrentNumberOfFiles
         {
             get
             {
-                return _fileCollection.View;
+                return _currentNumberOfFiles;
+            }
+            set
+            {
+                _currentNumberOfFiles = value;
+                OnPropertyChanged();
             }
         }
+
+        public string FileTypeFilter
+        {
+            get
+            {
+                return _fileTypeFilter;
+            }
+            set
+            {
+                _fileTypeFilter = value;
+                OnPropertyChanged();
+            }
+        }
+
         public string FilterText
         {
             get
@@ -122,6 +68,45 @@ namespace Find_Dupes.MVVM.ViewModel
                 OnPropertyChanged();
             }
         }
+
+        public ICommand GetAllFilesCommand
+        {
+            get;
+        }
+
+        public string SelectedItemListView
+        {
+            get
+            {
+                return _selectedItemListView;
+            }
+            set
+            {
+                _selectedItemListView = value;
+                OnPropertyChanged();
+                OnSelectedItemListViewChanged();
+            }
+        }
+
+        public ICollectionView SourceCollection
+        {
+            get
+            {
+                return _fileCollection.View;
+            }
+        }
+
+        private void ExecuteGetAllFiles()
+        {
+            VistaFolderBrowserDialog dialog = new VistaFolderBrowserDialog();
+            dialog.SelectedPath = "";
+            dialog.UseDescriptionForTitle = true;
+            dialog.ShowNewFolderButton = true;
+            dialog.Description = @"Select target folder";
+            dialog.ShowDialog();
+            GetAllFiles(dialog.SelectedPath);
+        }
+
         void FileCollectionFilter(object sender, FilterEventArgs e)
         {
             if (string.IsNullOrEmpty(FilterText))
@@ -140,6 +125,41 @@ namespace Find_Dupes.MVVM.ViewModel
             {
                 e.Accepted = false;
             }
+        }
+
+        private void GetAllFiles(string folderPath)
+        {
+            try
+            {
+                string[] allfiles = Directory.GetFiles(folderPath, "*.*", SearchOption.AllDirectories);
+                _myFiles.Clear();
+                foreach (string file in allfiles)
+                {
+                    _myFiles.Add(new MyFile(file));
+                }
+                CurrentNumberOfFiles = _myFiles.Count;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+        }
+
+        private void OnSelectedItemListViewChanged()
+        {
+            MyFile file = new MyFile(_selectedItemListView);
+            string temp;
+            int index = 0;
+            for (int i = file.Path.Length - 1; i >= 0; i--)
+            {
+                if (file.Path[i] == '\\')
+                {
+                    index = i;
+                    break;
+                }
+            }
+            Process.Start("explorer.exe", file.Path.Substring(0, index));
+            //Clipboard.SetText(file.Path.Substring(0,index));
         }
     }
 }
